ee3483da403df44fcf81405c81ac7c8c991cdd68
Bonginkosi Tlou <pb.tlou@gmail.com>
2026-07-26 14:50:58 +0200

    Added OpenFeignClient to call Employee-Service

--- src/main/java/com/bonginkosi/salaryservice/SalaryServiceApplication.java
+++ src/main/java/com/bonginkosi/salaryservice/SalaryServiceApplication.java
@@ -2,8 +2,10 @@ package com.bonginkosi.salaryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class SalaryServiceApplication {
 
 	public static void main(String[] args) {
