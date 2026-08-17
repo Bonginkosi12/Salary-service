dcad0f629a4bc37b5797ca26973aea024887fdfe
Bonginkosi Tlou <pb.tlou@gmail.com>
2026-08-17 21:10:47 +0200

    Added Method to create Salary record when an employee is created through Messaging Queue

--- src/main/java/com/bonginkosi/salaryservice/service/SalaryService.java
+++ src/main/java/com/bonginkosi/salaryservice/service/SalaryService.java
@@ -3,12 +3,13 @@ package com.bonginkosi.salaryservice.service;
 import com.bonginkosi.salaryservice.dto.SalaryDto;
 import com.bonginkosi.salaryservice.dto.EmployeeDto;
 import com.bonginkosi.salaryservice.entity.Salary;
+import com.bonginkosi.salaryservice.event.EmployeeCreatedEvent;
 import com.bonginkosi.salaryservice.repository.SalaryRepository;
 import feign.FeignException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
+import java.math.BigDecimal;
 
 @Service
 public class SalaryService {
@@ -30,10 +31,10 @@ public class SalaryService {
         EmployeeDto employee;
 
         try {
-            employee = userClient.getEmployeeById(salaryDto.getUserId());
+            employee = userClient.getEmployeeById(salaryDto.getEmployeeId());
         } catch (FeignException.NotFound ex) {
             throw new RuntimeException(
-                    "User with ID " + salaryDto.getUserId() + " was not found."
+                    "User with ID " + salaryDto.getEmployeeId() + " was not found."
             );
         }
 
@@ -45,11 +46,38 @@ public class SalaryService {
         salary.setDeductions(salaryDto.getDeductions());
         salary.setTax(salaryDto.getTax());
         salary.setOvertimePay(salaryDto.getOvertimePay());
-        salary.setUserId(employee.getId());
+        salary.setEmployeeId(employee.getId());
 
         salaryRepository.save(salary);
 
         return salaryDto;
     }
 
+    //Method to create Salary record when an employee is created
+    public void createSalaryForEmployee(EmployeeCreatedEvent event) {
+
+        Salary salary = new Salary();
+
+        salary.setEmployeeId(event.getId());
+
+        salary.setBasicSalary(BigDecimal.ZERO);
+        salary.setBonus(BigDecimal.ZERO);
+        salary.setDeductions(BigDecimal.ZERO);
+        salary.setTax(BigDecimal.ZERO);
+        salary.setOvertimePay(BigDecimal.ZERO);
+
+        salaryRepository.save(salary);
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
